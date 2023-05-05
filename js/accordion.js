f3da39233d8d4418de9e2fb1cf02780614af774e
Yauheni88 <86955351+Yauheni88@users.noreply.github.com>
2023-05-05 16:27:38 +0300

    Delete js directory

--- js/accordion.js
+++
@@ -1,9 +0,0 @@
-const questions = document.querySelectorAll(".accordion__question");
-// const arrows = document.querySelectorAll('.charitypage__person-question-arrow');
-
-questions.forEach( function (item) {
-    item.addEventListener('click', function () {
-        this.nextElementSibling.classList.toggle('active');
-        this.lastElementChild.classList.toggle('active');
-    })
-})
